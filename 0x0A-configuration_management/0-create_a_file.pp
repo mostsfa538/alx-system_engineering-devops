fc1c8845aa2dcd10806491bb507b68f1cdc15afb
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-19 18:54:24 +0200

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,11 @@
-# create a file in /tmp
+# create directory
 file { '/tmp':
-  ensure  => file,
+  ensure => 'directory',
+}
+
+# create a file in /tmp
+file { '/tmp/school':
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
