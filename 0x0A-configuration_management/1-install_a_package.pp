fd7147a838c5767103ae76d38dd6a4717e7af77c
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-19 21:28:10 +0200

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Install flask
-exec { 'install_flask':
-  command => 'pip3 install Flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
+package { 'flask':
+ensure   => '2.1.1',
+name     => 'flask',
+provider => 'pip3',
 }
