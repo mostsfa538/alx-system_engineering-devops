ca9da5da27add4b972c960c91fdcdf0720ce1648
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-19 21:31:40 +0200

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
+# install  Wereug
+package { 'Weruge':
+    ensure   => '2.1.1',
+    provider => 'apt',
+}
+
 # Install flask
 package { 'flask':
-ensure   => '2.1.1',
-name     => 'flask',
-provider => 'pip3',
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
