36c8563dc676bb5318a55d44e65576bc16fcd22a
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-20 19:18:12 +0300

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,39 +4,38 @@ import csv
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
     id = argv[1]
     url = 'https://jsonplaceholder.typicode.com/'
     user = requests.get(url + f"users/{id}").json()
     todo = requests.get(url + f"todos").json()
 
-    todo = [task for task in todo if task['userId'] == int(id)]
-    emp_name = user['name']
-    total_tasks = len(todo)
-    tasks = [task['title'] for task in todo if task['completed'] is True]
+    todos = [task for task in todo if task['userId'] == int(id)]
+    emp_name = user['username']
 
     print(
-        f'Employee {emp_name} is done with tasks({len(tasks)}/{total_tasks}):')
-    for task in tasks:
-        print(f'\t {task}')
+        f'Employee {emp_name} is done with tasks({len([task for task in todos if task["completed"]])}/{len(todos)}):')
+    for task in todos:
+        if task['completed']:
+            print(f'\t {task["title"]}')
 
-    tasksData = []
+    dataset = []
 
-    for task in tasks:
-        data = {}
-        data['ID'] = int(id)
-        data['Name'] = emp_name
-        data['Status'] = task[1]
-        data['task'] = task[0]
+    for task in todos:
+        data = {
+            "ID": id,
+            "Name": emp_name,
+            "Status": str(task["completed"]),
+            "task": task["title"]
+        }
+        dataset.append(data)
 
-        tasksData.append(data)
+    csv_filename = f'{id}.csv'
+    with open(csv_filename, 'w', newline='') as csvfile:
+        fieldnames = ["ID", "Name", "Status", "task"]
+        writer = csv.DictWriter(
+            csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
 
-    with open(f'{id}.csv', 'w') as csvfile:
-        fieldnames = tasksData[0].keys()
-
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-
-        for row in tasksData:
+        writer.writeheader()
+        for row in dataset:
             writer.writerow(row)
