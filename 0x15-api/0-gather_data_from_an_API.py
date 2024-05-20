4827d33c6a67a5bd98cea7b9a46f4facfb67346e
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-20 19:01:03 +0300

    0-gather_data_from_an_API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """ Here goes every thing """
 import requests
-import sys
+from sys import argv
 
 
-if __name__ == '__main__':
-    id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user_response = requests.get(url + f"users/{id}").json()
-    todo_response = requests.get(url + f"todo").json()
+if __name__ == "__main__":
+    id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + f"users/{id}").json()
+    todo = requests.get(url + f"todos").json()
 
-    todo_response = [
-        task for task in todo_response if task['userId'] == int(id)]
-    emp_name = user_response['name']
-    total_tasks = len(todo_response)
-    tasks = [task['title']
-             for task in todo_response if task['completed'] is True]
+    todo = [task for task in todo if task['userId'] == int(id)]
+    emp_name = user['name']
+    total_tasks = len(todo)
+    tasks = [task['title'] for task in todo if task['completed'] is True]
 
     print(
         f'Employee {emp_name} is done with tasks({len(tasks)}/{total_tasks}):')
