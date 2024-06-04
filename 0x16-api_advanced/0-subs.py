832930b9609505e9232cd43435c17558c980960e
mostsfa538 <mostafa51moktar@gmail.com>
2024-06-04 12:44:59 +0300

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" How many subs """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        that queries the Reddit API and returns the number of subscribers
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user = {'User-agent': 'Google Chrome Version 118.0.5993.120'}
+    response = requests.get(url, headers=user, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subs = data['data']['subscribers']
+        return (subs)
+
+    else:
+        return (0)
