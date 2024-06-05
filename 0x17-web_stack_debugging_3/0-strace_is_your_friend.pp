1b345d4c7876461080541252b90f60bb3884c7d2
mostsfa538 <mostafa51moktar@gmail.com>
2024-06-05 14:18:12 +0300

    strace_is_your_friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,9 @@
+# Solving Apche returing error code 500
+
+# the problem is that in the file /var/www/html/wp-settings.php
+# in line 137: require_once( ABSPATH . WPINC . '/class-wp-locale-switcher.php' );
+# the file name "class-wp-locale-switcher.php" was written as "class-wp-locale-switcher.phpp"
+# which is the file does not exits.
 exec{'fix-syntax':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
     path    =>  ['/bin', '/usr/bin']
